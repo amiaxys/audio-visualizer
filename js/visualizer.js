fb3390add6879ddc21ef1853009e6803e7a27268
amiaxys <amyuqiliang@gmail.com>
2023-02-16 17:35:38 -0500

    feat: first commit

---
+++ js/visualizer.js
@@ -0,0 +1,53 @@
+(function () {
+  "use strict";
+  document.getElementById("start").addEventListener("click", function () {
+    const canvas = document.getElementById("visualizer");
+    const canvasCtx = canvas.getContext("2d");
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+
+    const audioElmt = document.getElementById("source");
+    const audioCtx = new AudioContext();
+    const analyser = audioCtx.createAnalyser();
+    const distortion = audioCtx.createWaveShaper();
+    const source = audioCtx.createMediaElementSource(audioElmt);
+
+    source.connect(analyser);
+    analyser.connect(distortion);
+    distortion.connect(audioCtx.destination);
+
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    analyser.getByteTimeDomainData(dataArray);
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    function draw() {
+      requestAnimationFrame(draw);
+      analyser.getByteTimeDomainData(dataArray);
+      canvasCtx.fillStyle = "rgb(220, 230, 240)";
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = "rgb(200, 100, 50)";
+      canvasCtx.beginPath();
+      const sliceWidth = WIDTH / bufferLength;
+      let x = 0;
+      for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = v * (HEIGHT / 2);
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+      canvasCtx.lineTo(WIDTH, HEIGHT / 2);
+      canvasCtx.stroke();
+    }
+    draw();
+  });
+})();
